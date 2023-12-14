a3b52ee0aabdf6e4bf6e972acb4fda3802db2b83
이하성 <tedsoftj1123@gmail.com>
2023-12-14 16:18:25 +0900

    setting: (#3) common, global exceptions

---
+++ src/main/kotlin/org/example/chitchatserver/common/exception/CommonExceptions.kt
@@ -0,0 +1,27 @@
+package org.example.chitchatserver.common.exception
+
+import org.example.chitchatserver.global.exception.ResponseStatus.BAD_REQUEST
+import org.example.chitchatserver.global.exception.ResponseStatus.CONFLICT
+import org.example.chitchatserver.global.exception.ResponseStatus.FORBIDDEN
+import org.example.chitchatserver.global.exception.ResponseStatus.NOT_FOUND
+import org.example.chitchatserver.global.exception.ResponseStatus.UNAUTHORIZED
+
+class NotFoundException(
+    message: String
+) : CustomException(NOT_FOUND, message)
+
+class BadRequestException(
+    message: String
+) : CustomException(BAD_REQUEST, message)
+
+class UnauthorizedException(
+    message: String
+) : CustomException(UNAUTHORIZED, message)
+
+class ForbiddenException(
+    message: String
+) : CustomException(FORBIDDEN, message)
+
+class ConflictException(
+    message: String
+) : CustomException(CONFLICT, message)
---
+++ src/main/kotlin/org/example/chitchatserver/global/exception/GlobalExceptions.kt
@@ -0,0 +1,9 @@
+package org.example.chitchatserver.global.exception
+
+import org.example.chitchatserver.common.exception.CustomException
+import org.example.chitchatserver.global.exception.ResponseStatus.INTERNAL_SERVER_ERROR
+
+
+object InternalServerError : CustomException(INTERNAL_SERVER_ERROR, "Internal Server Error") {
+    private fun readResolve(): Any = InternalServerError
+}
