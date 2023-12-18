b4a72728816c0c5c1ceae1440a2d3f58ab60cd1e
이하성 <tedsoftj1123@gmail.com>
2023-12-18 09:23:51 +0900

    setting: (#5) r2dbc, security library

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,11 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("io.jsonwebtoken:jjwt:0.12.3")
+    implementation("org.postgresql:r2dbc-postgresql:1.0.3.RELEASE")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.1.0")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.0")
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc:3.2.0")
     implementation ("org.springframework.boot:spring-boot-starter-validation")
     implementation("mysql:mysql-connector-java:8.0.28")
     implementation("com.querydsl:querydsl-jpa:5.0.0")
