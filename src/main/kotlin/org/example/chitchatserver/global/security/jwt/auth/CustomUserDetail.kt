f8a96e196d4585d5695083895a3b9586f7a24ee2
이하성 <tedsoftj1123@gmail.com>
2023-12-18 09:25:51 +0900

    feat: (#5) custom user detail service

---
+++ src/main/kotlin/org/example/chitchatserver/global/security/jwt/auth/CustomUserDetail.kt
@@ -0,0 +1,27 @@
+package org.example.chitchatserver.global.security.jwt.auth
+
+import org.example.chitchatserver.domain.user.persistence.Authority
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import java.util.*
+
+class CustomUserDetail(
+    private val userId: UUID,
+    private val authority: Authority
+) : UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> =
+        mutableListOf(SimpleGrantedAuthority(authority.name))
+
+    override fun getPassword(): String? = null
+
+    override fun getUsername(): String = userId.toString()
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+}
---
+++ src/main/kotlin/org/example/chitchatserver/global/security/jwt/auth/CustomUserDetailService.kt
@@ -0,0 +1,25 @@
+package org.example.chitchatserver.global.security.jwt.auth
+
+import org.example.chitchatserver.domain.user.persistence.repository.UserRepository
+import org.example.chitchatserver.global.exception.InvalidTokenException
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+import java.util.*
+
+@Component
+class CustomUserDetailService(
+    private val userRepository: UserRepository
+) : ReactiveUserDetailsService {
+
+    override fun findByUsername(username: String?): Mono<UserDetails> =
+        userRepository.findById(UUID.fromString(username))
+            .switchIfEmpty(Mono.error(InvalidTokenException))
+            .map {
+                CustomUserDetail(
+                    userId = it.id,
+                    authority = it.authority
+                )
+            }
+}
