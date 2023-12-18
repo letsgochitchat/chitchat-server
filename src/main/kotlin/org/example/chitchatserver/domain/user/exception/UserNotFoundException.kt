9d20180391284a1b7b6d779679f6382874a6b9fb
이하성 <tedsoftj1123@gmail.com>
2023-12-18 09:39:31 +0900

    refactor: (#5) use response status

--- src/main/kotlin/org/example/chitchatserver/domain/user/exception/UserNotFoundException.kt
+++ src/main/kotlin/org/example/chitchatserver/domain/user/exception/UserNotFoundException.kt
@@ -1,7 +1,8 @@
 package org.example.chitchatserver.domain.user.exception
 
 import org.example.chitchatserver.common.exception.CustomException
+import org.example.chitchatserver.common.exception.ResponseStatus.NOT_FOUND
 
-object UserNotFoundException : CustomException(404, "User Not Found") {
+object UserNotFoundException : CustomException(NOT_FOUND, "User Not Found") {
     private fun readResolve(): Any = UserNotFoundException
 }
--- src/main/kotlin/org/example/chitchatserver/global/exception/TokenExceptions.kt
+++ src/main/kotlin/org/example/chitchatserver/global/exception/TokenExceptions.kt
@@ -1,15 +1,16 @@
 package org.example.chitchatserver.global.exception
 
 import org.example.chitchatserver.common.exception.CustomException
+import org.example.chitchatserver.common.exception.ResponseStatus.UNAUTHORIZED
 
-object InvalidTokenException : CustomException(401, "Invalid Token") {
+object InvalidTokenException : CustomException(UNAUTHORIZED, "Invalid Token") {
     private fun readResolve(): Any = InvalidTokenException
 }
 
-object ExpiredTokenException : CustomException(401, "Expired Token") {
+object ExpiredTokenException : CustomException(UNAUTHORIZED, "Expired Token") {
     private fun readResolve(): Any = ExpiredTokenException
 }
 
-object UnexpectedTokenException : CustomException(401, "Unexpected Token") {
+object UnexpectedTokenException : CustomException(UNAUTHORIZED, "Unexpected Token") {
     private fun readResolve(): Any = UnexpectedTokenException
 }
