479efb91726519adb3e7b6531b28a9c0ef6cff05
이하성 <tedsoftj1123@gmail.com>
2023-12-18 09:24:23 +0900

    chore: (#5) to common

--- src/main/kotlin/org/example/chitchatserver/common/exception/CommonExceptions.kt
+++ src/main/kotlin/org/example/chitchatserver/common/exception/CommonExceptions.kt
@@ -1,10 +1,10 @@
 package org.example.chitchatserver.common.exception
 
-import org.example.chitchatserver.global.exception.ResponseStatus.BAD_REQUEST
-import org.example.chitchatserver.global.exception.ResponseStatus.CONFLICT
-import org.example.chitchatserver.global.exception.ResponseStatus.FORBIDDEN
-import org.example.chitchatserver.global.exception.ResponseStatus.NOT_FOUND
-import org.example.chitchatserver.global.exception.ResponseStatus.UNAUTHORIZED
+import org.example.chitchatserver.common.exception.ResponseStatus.BAD_REQUEST
+import org.example.chitchatserver.common.exception.ResponseStatus.CONFLICT
+import org.example.chitchatserver.common.exception.ResponseStatus.FORBIDDEN
+import org.example.chitchatserver.common.exception.ResponseStatus.NOT_FOUND
+import org.example.chitchatserver.common.exception.ResponseStatus.UNAUTHORIZED
 
 class NotFoundException(
     message: String
