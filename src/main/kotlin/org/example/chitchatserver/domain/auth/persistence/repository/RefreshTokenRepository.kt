4d25718f8d2db27650cda024b726d42aa71e9847
gilgeunwoo <kesksjxhx@gmail.com>
2023-12-29 11:12:41 +0900

    refactor: (#6) DIP

--- src/main/kotlin/org/example/chitchatserver/domain/auth/persistence/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/org/example/chitchatserver/domain/auth/persistence/repository/RefreshTokenRepository.kt
@@ -1,20 +1,8 @@
 package org.example.chitchatserver.domain.auth.persistence.repository
 
 import org.example.chitchatserver.domain.auth.persistence.RefreshTokenEntity
-import org.springframework.data.redis.core.ReactiveRedisOperations
-import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
-import java.time.Duration
 
-@Repository
-class RefreshTokenRepository(
-    private val reactiveRedisOperations: ReactiveRedisOperations<String, Any>
-) {
-
-    fun save(refreshTokenEntity: RefreshTokenEntity): Mono<Boolean> =
-        reactiveRedisOperations.opsForValue().set(
-            refreshTokenEntity.token,
-            refreshTokenEntity,
-            Duration.ofSeconds(refreshTokenEntity.ttl.toLong())
-        )
+interface RefreshTokenRepository {
+    fun save(refreshTokenEntity: RefreshTokenEntity): Mono<Boolean>
 }
---
+++ src/main/kotlin/org/example/chitchatserver/domain/auth/persistence/repository/impl/RefreshTokenRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package org.example.chitchatserver.domain.auth.persistence.repository.impl
+
+import org.example.chitchatserver.domain.auth.persistence.RefreshTokenEntity
+import org.example.chitchatserver.domain.auth.persistence.repository.RefreshTokenRepository
+import org.springframework.data.redis.core.ReactiveRedisOperations
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
+import java.time.Duration
+
+@Repository
+class RefreshTokenRepositoryImpl(
+    private val reactiveRedisOperations: ReactiveRedisOperations<String, Any>
+) : RefreshTokenRepository {
+
+    override fun save(refreshTokenEntity: RefreshTokenEntity): Mono<Boolean> =
+        reactiveRedisOperations.opsForValue().set(
+            refreshTokenEntity.token,
+            refreshTokenEntity,
+            Duration.ofSeconds(refreshTokenEntity.ttl.toLong())
+        )
+}
