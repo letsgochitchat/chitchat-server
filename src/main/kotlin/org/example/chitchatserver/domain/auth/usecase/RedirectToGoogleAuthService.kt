f6a02815d7561b9c951b19e7a43fabd62527c624
길근우 <kesksjxhx@gmail.com>
2024-01-02 10:25:10 +0900

    feat: (#10) oauth login api

---
+++ src/main/kotlin/org/example/chitchatserver/domain/auth/usecase/RedirectToGoogleAuthService.kt
@@ -0,0 +1,21 @@
+package org.example.chitchatserver.domain.auth.usecase
+
+import org.example.chitchatserver.thirdparty.webclient.google.GoogleWebClient
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+import java.net.URI
+
+@Service
+class RedirectToGoogleAuthService(
+    private val googleWebClient: GoogleWebClient,
+) {
+
+    fun execute(): ResponseEntity<Void> {
+        val headers = HttpHeaders()
+        headers.location = URI.create(googleWebClient.getGoogleAuthUrl())
+        return ResponseEntity(headers, HttpStatus.MOVED_PERMANENTLY)
+    }
+}
