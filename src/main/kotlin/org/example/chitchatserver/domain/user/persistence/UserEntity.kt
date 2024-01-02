8d02b1c3b04ece72d8329c3b6b907754a65b5ba3
길근우 <kesksjxhx@gmail.com>
2024-01-02 10:24:33 +0900

    setting: (#10) user entity

--- src/main/kotlin/org/example/chitchatserver/domain/user/persistence/UserEntity.kt
+++ src/main/kotlin/org/example/chitchatserver/domain/user/persistence/UserEntity.kt
@@ -1,26 +1,32 @@
 package org.example.chitchatserver.domain.user.persistence
 
+import org.example.chitchatserver.common.entity.BaseUUIDEntity
 import org.springframework.data.annotation.Id
+import org.springframework.data.domain.Persistable
 import org.springframework.data.relational.core.mapping.Table
 import java.util.UUID
 
 @Table(name = "tbl_user")
 class UserEntity(
-    @Id
-    val id: UUID,
-
-    val accountId: String,
+    @get:JvmName("getIdentifier")
+    override var id: UUID = UUID(0, 0),
 
     val email: String,
 
-    val password: String,
-
     val profileImageUrl: String,
 
+    val sub: String,
+
+    val authType: AuthType,
+
     val authority: Authority
-)
+) : BaseUUIDEntity(id)
 
 enum class Authority {
     USER,
     ADMIN
+}
+
+enum class AuthType {
+    GOOGLE
 }
--- src/main/kotlin/org/example/chitchatserver/domain/user/persistence/repository/UserRepository.kt
+++ src/main/kotlin/org/example/chitchatserver/domain/user/persistence/repository/UserRepository.kt
@@ -2,6 +2,9 @@ package org.example.chitchatserver.domain.user.persistence.repository
 
 import org.example.chitchatserver.domain.user.persistence.UserEntity
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import reactor.core.publisher.Mono
 import java.util.UUID
 
-interface UserRepository : ReactiveCrudRepository<UserEntity, UUID>
+interface UserRepository : ReactiveCrudRepository<UserEntity, UUID> {
+    fun findBySub(sub: String): Mono<UserEntity>
+}
