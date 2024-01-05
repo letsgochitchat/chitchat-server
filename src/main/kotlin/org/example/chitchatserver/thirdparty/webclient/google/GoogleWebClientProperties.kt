cd34c75de244bfed5d0591688458f855190c38bb
길근우 <kesksjxhx@gmail.com>
2024-01-05 10:18:29 +0900

    refactor: (#10) yml 수정

--- src/main/kotlin/org/example/chitchatserver/thirdparty/webclient/google/GoogleWebClientProperties.kt
+++ src/main/kotlin/org/example/chitchatserver/thirdparty/webclient/google/GoogleWebClientProperties.kt
@@ -3,7 +3,7 @@ package org.example.chitchatserver.thirdparty.webclient.google
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.bind.ConstructorBinding
 
-@ConfigurationProperties("google")
+@ConfigurationProperties("oauth.google")
 class GoogleWebClientProperties @ConstructorBinding constructor(
     val clientId: String,
     val clientSecret: String,
