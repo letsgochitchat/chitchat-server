8795f068039c1cb81477d6e3f3a3dd756637aa1c
길근우 <kesksjxhx@gmail.com>
2024-01-05 10:19:19 +0900

    refactor: (#10) unused import

--- src/main/kotlin/org/example/chitchatserver/domain/auth/usecase/RedirectToGoogleAuthService.kt
+++ src/main/kotlin/org/example/chitchatserver/domain/auth/usecase/RedirectToGoogleAuthService.kt
@@ -5,7 +5,6 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
-import reactor.core.publisher.Mono
 import java.net.URI
 
 @Service
