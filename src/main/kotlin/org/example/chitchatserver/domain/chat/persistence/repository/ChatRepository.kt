01d4a95dd82c99ff9277c77086d815e435d6e780
길근우 <kesksjxhx@gmail.com>
2024-01-15 22:28:56 +0900

    feat: (#14) findAllByRoomIdOrderBySendAtDesc

--- src/main/kotlin/org/example/chitchatserver/domain/chat/persistence/repository/ChatRepository.kt
+++ src/main/kotlin/org/example/chitchatserver/domain/chat/persistence/repository/ChatRepository.kt
@@ -2,7 +2,9 @@ package org.example.chitchatserver.domain.chat.persistence.repository
 
 import org.example.chitchatserver.domain.chat.persistence.ChatEntity
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import reactor.core.publisher.Flux
 import java.util.UUID
 
 interface ChatRepository : ReactiveMongoRepository<ChatEntity, UUID> {
+    fun findAllByRoomIdOrderBySendAtDesc(roomId: UUID): Flux<ChatEntity>
 }
