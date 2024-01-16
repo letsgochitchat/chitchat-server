1fc7a9879741fe25ad618ecd7a12cc2b64b8e105
길근우 <kesksjxhx@gmail.com>
2024-01-16 14:19:19 +0900

    feat: (#14) pageable

--- src/main/kotlin/org/example/chitchatserver/domain/chat/persistence/repository/ChatRepository.kt
+++ src/main/kotlin/org/example/chitchatserver/domain/chat/persistence/repository/ChatRepository.kt
@@ -1,10 +1,11 @@
 package org.example.chitchatserver.domain.chat.persistence.repository
 
 import org.example.chitchatserver.domain.chat.persistence.ChatEntity
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import reactor.core.publisher.Flux
 import java.util.UUID
 
 interface ChatRepository : ReactiveMongoRepository<ChatEntity, UUID> {
-    fun findAllByRoomIdOrderBySendAtDesc(roomId: UUID): Flux<ChatEntity>
+    fun findAllByRoomIdOrderBySendAtDesc(roomId: UUID, pageable: Pageable): Flux<ChatEntity>
 }
