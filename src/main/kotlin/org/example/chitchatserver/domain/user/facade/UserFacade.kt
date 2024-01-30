26b5025c27fc88386d7a28256287971b055982ac
이하성 <tedsoftj1123@gmail.com>
2024-01-30 13:30:40 +0900

    feat: (#9) current user 정보 조회

--- src/main/kotlin/org/example/chitchatserver/domain/user/facade/UserFacade.kt
+++ src/main/kotlin/org/example/chitchatserver/domain/user/facade/UserFacade.kt
@@ -1,5 +1,8 @@
 package org.example.chitchatserver.domain.user.facade
 
+import org.example.chitchatserver.domain.user.persistence.UserEntity
+import org.example.chitchatserver.global.exception.InvalidTokenException
+import org.example.chitchatserver.global.security.jwt.auth.CustomUserDetail
 import org.springframework.security.core.context.ReactiveSecurityContextHolder
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
@@ -10,4 +13,9 @@ class UserFacade {
 
     fun getCurrentUserId(): Mono<UUID> = ReactiveSecurityContextHolder.getContext()
         .flatMap { Mono.just(UUID.fromString(it.authentication.name)) }
+        .switchIfEmpty(Mono.error(InvalidTokenException))
+
+    fun getCurrentUser(): Mono<UserEntity> = ReactiveSecurityContextHolder.getContext()
+        .map { (it.authentication.principal as CustomUserDetail).user }
+        .switchIfEmpty(Mono.error(InvalidTokenException))
 }
