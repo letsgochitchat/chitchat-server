1af2339efdbacd447830146b20272932863af0f7
이하성 <tedsoftj1123@gmail.com>
2024-01-30 13:31:05 +0900

    refactor: (#9) 채팅기능 리팩토링

--- src/main/kotlin/org/example/chitchatserver/domain/chat/presentation/event/Message.kt
+++ src/main/kotlin/org/example/chitchatserver/domain/chat/presentation/event/Message.kt
@@ -1,16 +1,15 @@
 package org.example.chitchatserver.domain.chat.presentation.event
 
+import org.example.chitchatserver.domain.chat.persistence.ChatType
 import java.time.LocalDateTime
+import java.util.UUID
 
 data class Message(
-    val type: MessageType,
+    val type: ChatType,
     val content: String,
     val sendAt: LocalDateTime = LocalDateTime.now(),
-    val roomId: String? = null,
+    val roomId: UUID? = null,
     val nickname: String,
-    val profileImageUrl: String
+    val profileImageUrl: String,
+    var userId: UUID? = null
 )
-
-enum class MessageType {
-    MESSAGE, IMAGE, CONNECTION, NEXT, DISCONNECT
-}
--- src/main/kotlin/org/example/chitchatserver/domain/chat/usecase/ChatService.kt
+++ src/main/kotlin/org/example/chitchatserver/domain/chat/usecase/ChatService.kt
@@ -1,62 +1,94 @@
 package org.example.chitchatserver.domain.chat.usecase
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import kotlinx.coroutines.reactor.awaitSingle
-import kotlinx.coroutines.reactor.mono
-import org.example.chitchatserver.domain.chat.persistence.repository.RoomRepository
+import org.example.chitchatserver.common.exception.BadRequestException
+import org.example.chitchatserver.domain.chat.persistence.ChatEntity
+import org.example.chitchatserver.domain.chat.persistence.ChatRepository
+import org.example.chitchatserver.domain.chat.persistence.ChatType
+import org.example.chitchatserver.domain.chat.persistence.RoomEntity
+import org.example.chitchatserver.domain.chat.persistence.RoomRepository
 import org.example.chitchatserver.domain.chat.presentation.event.Message
-import org.example.chitchatserver.domain.chat.presentation.event.MessageType
-import org.example.chitchatserver.global.security.facade.SecurityFacade
+import org.example.chitchatserver.domain.user.facade.UserFacade
+import org.example.chitchatserver.domain.user.persistence.UserEntity
 import org.springframework.data.redis.core.ReactiveRedisOperations
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.socket.WebSocketMessage
 import org.springframework.web.reactive.socket.WebSocketSession
 import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
+import reactor.kotlin.core.util.function.component1
+import reactor.kotlin.core.util.function.component2
+import java.util.UUID
 
 @Service
 class ChatService(
     private val roomRepository: RoomRepository,
-    private val redisOperations: ReactiveRedisOperations<String, String>,
-    private val securityFacade: SecurityFacade,
+    private val redisOperations: ReactiveRedisOperations<String, Any>,
+    private val chatRepository: ChatRepository,
+    private val userFacade: UserFacade,
     private val objectMapper: ObjectMapper
 ) {
 
     fun execute(session: WebSocketSession): Mono<Void> =
-        mono {
-            val room = roomRepository.findFirstByOrderByConnectionCountDesc().awaitSingle()
-            val roomId = room.id.toString()
-            val user = securityFacade.getCurrentUser().awaitSingle()
+        roomRepository.queryMinConnectionCountRoom()
+            .zipWith(userFacade.getCurrentUser())
+            .flatMap { (room, currentUser) ->
+                val updateRoomOp = roomRepository.save(room.increaseConnectionCount())
+                val chatOperations = Mono.zip(
+                    receiveMessage(session, room.id, currentUser.id),
+                    sendMessage(session, room, currentUser)
+                )
 
-            Mono.zip(
-                session.receive()
-                    .map { it.toMessage() }
-                    .flatMap {
-                        redisOperations.convertAndSend(roomId, it.toJSON())
-                    }.then(),
-                session.send(
-                    Mono.just(
-                        session.textMessage(
-                            Message(
-                                type = MessageType.CONNECTION,
-                                content = roomId,
-                                nickname = user.email,
-                                profileImageUrl = user.profileImageUrl,
-                                roomId = roomId
-                            ).toJSON()
-                        )
-                    )
-                ).thenMany(
-                    session.send(
-                        redisOperations.listenToChannel(roomId)
-                            .map { session.textMessage(it.message) }
-                    )
-                ).then()
-            ).then().awaitSingle()
-        }
+                updateRoomOp.then(chatOperations)
+                    .publishOn(Schedulers.boundedElastic())
+                    .doFinally { roomRepository.save(room.decreaseConnectionCount()).subscribe() }
+            }
+            .then()
 
-    fun WebSocketMessage.toMessage(): Message =
-        objectMapper.readValue(payloadAsText, Message::class.java)
+    private fun receiveMessage(session: WebSocketSession, roomId: UUID, currentUserId: UUID): Mono<Void> =
+        session.receive()
+            .flatMap {
+                val message = it.toMessage(currentUserId)
+                val publishOp = redisOperations.convertAndSend(roomId.toString(), message.toJSON())
+                val saveOp = chatRepository.save(message.toChatEntity(roomId))
+                publishOp.then(saveOp)
+            }
+            .then()
+
+    private fun sendMessage(session: WebSocketSession, room: RoomEntity, user: UserEntity): Mono<Void> {
+        val connectionMessage = Message(
+            type = ChatType.CONNECTION,
+            content = room.connectionCount.toString(),
+            nickname = user.email,
+            profileImageUrl = user.profileImageUrl,
+            roomId = room.id
+        ).toJSON()
+
+        val sendConnectionMessage = session.send(Mono.just(session.textMessage(connectionMessage)))
+        val sendChannelMessage = redisOperations.listenToChannel(room.id.toString())
+            .map { objectMapper.readValue(it.message as String, Message::class.java) }
+            .filter { it.userId != user.id }
+            .map { session.textMessage(it.toJSON()) }
+
+        return sendConnectionMessage.thenMany(session.send(sendChannelMessage)).then()
+    }
+
+    fun WebSocketMessage.toMessage(currentUserId: UUID): Message = try {
+        objectMapper.readValue(payloadAsText, Message::class.java).apply { this.userId = currentUserId }
+    } catch (e: Exception) {
+        throw BadRequestException("Invalid Message Format")
+    }
 
     fun Message.toJSON(): String =
         objectMapper.writeValueAsString(this)
+
+    fun Message.toChatEntity(roomId: UUID) =
+        ChatEntity(
+            userId = this.userId!!,
+            roomId = roomId,
+            content = this.content,
+            nickname = this.nickname,
+            profileImageUrl = this.profileImageUrl,
+            type = this.type
+        )
 }
